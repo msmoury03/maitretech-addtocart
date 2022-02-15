0aed13034e6cc5dbc51543dfc98ea8bb8e300de2
msmoury03 <maneesingh20@gmail.com>
2022-02-15 09:29:15 +0530

    new

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -16,9 +16,7 @@ const Product = ({item,handlclk}) => {
                   <button onClick={()=>handlclk(item)} class=" w-16 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border  rounded m-1">
                     +
                   </button>
-                  <button class="w-16 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border  rounded m-1">
-                    -
-                  </button>
+                  
                 </div>
               </div>
             </div>
